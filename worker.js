f9c0694fa6be6f12e746744bcb5bbaa0c76625bb
llmfmym-commits <llmfmym@gmail.com>
2026-08-21 14:10:12 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,24 +1,5 @@
 export default {
   async fetch(request, env) {
-
-    const url = new URL(request.url);
-
-    // تست Worker
-    if (url.pathname === "/api/test") {
-      return new Response(
-        JSON.stringify({
-          ok: true,
-          message: "APK Builder API is working"
-        }),
-        {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-    }
-
-    // فعلاً بقیه درخواست‌ها را به فایل‌های سایت بده
-    return env.ASSETS.fetch(request);
+    return new Response("APK Builder Worker OK");
   }
 };
